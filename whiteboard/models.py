799470362d7d140e2bda23c0d49cd1432a85907a
Kamide <dengkangming@gmail.com>
2019-11-07 23:00:32 -0500

    Create Student, Teacher models

--- whiteboard/models.py
+++ whiteboard/models.py
@@ -12,6 +12,21 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+class Department(db.Model):
+    __tablename__ = 'Departments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    chair = db.Column(db.String(255), nullable=False)
+    office = db.Column(db.String(255), nullable=False)
+
+
+class Major(db.Model):
+    __tablename__ = 'Majors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    department = db.Column(db.Integer, db.ForeignKey('Departments.id'), nullable=False)
+
+
 class User(db.Model, UserMixin):
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
@@ -20,8 +35,26 @@ class User(db.Model, UserMixin):
     full_name = db.Column(db.String(255), nullable=False)
     display_name = db.Column(db.String(255))
     email = db.Column(db.String(254, collation='NOCASE'), nullable=False, unique=True)
-    join_date = db.Column(db.Date())
+    join_date = db.Column(db.DateTime())
 
     @property
     def is_active(self):
         return self.join_date is not None
+
+    @property
+    def is_teacher(self):
+        return Teacher.query.filter_by(user=self.id).first()
+
+
+class Teacher(db.Model):
+    __tablename__ = 'Teachers'
+    user = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
+    department = db.Column(db.Integer, db.ForeignKey('Departments.id'))
+    office = db.Column(db.String(255))
+    office_hours = db.Column(db.String(255))
+
+
+class Student(db.Model):
+    __tablename__ = 'Students'
+    user = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
+    major = db.Column(db.Integer, db.ForeignKey('Majors.id'))
